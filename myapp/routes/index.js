b77753955fa1da2a63e268b6aec23a1fd9bc2093
zhuangrong <42589963@qq.com>
2017-07-13 16:44:01 +0800

    创建express服务端，调用node-sass-middleware实现服务端实时编译scss文件

---
+++ myapp/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //res.render('index.html', { title: 'Express112' });
+ // res.sendFile( __dirname + 'index.html');
+  //console.log(__dirname + 'index.html')
+  res.sendFile(__dirname + '/index.html')
+});
+
+module.exports = router;
